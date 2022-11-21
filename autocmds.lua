8ebe1af39679148d1daab53aee242c443ef2c2d6
Phusit Somboonyingsuk <phusitsom@gmail.com>
2022-11-21 16:12:18 +0700

    show diagnostic on cursor hold

--- autocmds.lua
+++ autocmds.lua
@@ -2,3 +2,8 @@ vim.api.nvim_create_autocmd(
   { "BufRead", "BufNewFile" },
   { pattern = { "*.txt", "*.md", "*.tex" }, command = "setlocal spell" }
 )
+
+vim.api.nvim_create_autocmd(
+  { "CursorHold" },
+  { pattern = { "*" }, command = "lua vim.diagnostic.open_float(nil, {focusable = false})" }
+)
