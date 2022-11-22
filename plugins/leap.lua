5015d80e9f6d5b0f93e721dd347a864b8c89cb94
Phusit Somboonyingsuk <phusitsom@gmail.com>
2022-11-22 17:03:18 +0700

    Remove custom leap highlighting

--- plugins/leap.lua
+++ plugins/leap.lua
@@ -1,9 +1,1 @@
 require("leap").add_default_mappings()
-
-vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
-
-vim.api.nvim_set_hl(0, "LeapMatch", {
-  fg = "white",
-  bold = true,
-  nocombine = true,
-})
